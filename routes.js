619734b8d9f2691b292b5f05ca567bd4c71abcc9
Eduardo Zagalsky <zagalsky.eduardo@gmail.com>
2023-05-01 02:57:36 -0500

    added the routes

---
+++ routes.js
@@ -0,0 +1,20 @@
+const express = require('express');
+const router = express.Router();
+
+router.get('/', function (req, res) {
+
+})
+router.post('/', function (req, res) {
+
+})
+router.get('/:name', function (req, res) {
+
+})
+router.patch('/:name', function (req, res) {
+
+})
+router.delete('/:name', function (req, res) {
+
+})
+
+module.exports = router;
